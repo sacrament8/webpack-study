f27e988cdec6c2ed9f553d90845110d5c78691c1
sacrament <xkinked.tailx@gmail.com>
2020-02-14 19:41:11 +0900

    implement some modules

---
+++ src/utilities.js
@@ -0,0 +1,12 @@
+export function NiJou(num) {
+  return num ** 2;
+}
+
+export const NAME = 'takuya';
+
+export let hoge = 'hahaha'
+export default class Cat {
+  static say() {
+    return 'nyaaaaaan';
+  }
+}
